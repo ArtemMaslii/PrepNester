e184a6313dc47b99b29f19d7b7d3a4c051f64df8
Artem Maslii <maslijartei@gmail.com>
2025-03-27 20:07:41 +0200

    Initial setup

---
+++ src/test/java/com/project/prepnester/PrepnesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prepnester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepnesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
