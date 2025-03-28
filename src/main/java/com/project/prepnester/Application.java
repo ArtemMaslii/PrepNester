fa5134a44237da3162777a306f00b4ab9417eaaa
Artem Maslii <maslijartei@gmail.com>
2025-03-28 20:27:33 +0200

    Add Docker

--- src/main/java/com/project/prepnester/Application.java
+++ src/main/java/com/project/prepnester/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrepnesterApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrepnesterApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
---
+++ src/test/java/com/project/prepnester/TestApplication.java
@@ -0,0 +1,8 @@
+package com.project.prepnester;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.project.prepnester")
+public class TestApplication {
+
+}
