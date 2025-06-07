5786aa6a645f9f98acd4b2e62785e139c6af1eef
Artem Maslii <maslijartei@gmail.com>
2025-06-07 17:19:56 +0300

    Alignment

--- src/main/resources/db/db.changelog.test.injection.sql
+++ src/main/resources/db/db.changelog.test.injection.sql
@@ -124,11 +124,7 @@ VALUES ('784bfa2b-6fc4-4288-a8ae-0579c4cc0b18', 'f03236c3-5746-4d2b-9c97-6b93c34
 INSERT INTO categories_questions (category_id, question_id)
 VALUES ('1e52f3b1-b2cf-470d-8338-bdbba6988e7b',
         '784bfa2b-6fc4-4288-a8ae-0579c4cc0b18'),
-       ('1e52f3b1-b2cf-470d-8338-bdbba6988e7b', '784bfa2b-6fc4-4288-a8ae-0579c4cc0b17'),
+       ('1e52f3b1-b2cf-470d-8338-bdbba6988e7b',
+        '784bfa2b-6fc4-4288-a8ae-0579c4cc0b17'),
        ('e36fdba0-f12d-4a51-83a9-60e37452a831',
         '9345ef2f-cd8f-4eae-97cc-6f4e81ed995d');
-
-
---- my bad due to assuming that this work would be perspective
--- mentioned functional non functional requirements
--- use words for use case diagram on presentation
